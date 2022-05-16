0dce5ae992c9edd2e4fcd7c0fb03252608c52577
ShinjungOh <tlswjd9260@gmail.com>
2022-05-17 08:48:17 +0900

    [ADD] 인프런: 기본문제 - 연필 개수

---
+++ inflearn/normal/연필 개수/solution.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer;
+    answer = Math.ceil(n / 12);
+    return answer;
+}
+
+console.log(solution(25));
+console.log(solution(178));
+
+//https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math
