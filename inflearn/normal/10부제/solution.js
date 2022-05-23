6efd02ca7fc967259138e31df472da2f3b010427
ShinjungOh <tlswjd9260@gmail.com>
2022-05-23 12:26:31 +0900

    [ADD] 인프런: 기본문제 - 10부제

---
+++ inflearn/normal/10부제/solution.js
@@ -0,0 +1,16 @@
+function solution(day, arr) {
+    let answer = 0;
+    for (let x of arr) {
+        if (x % 10 === day) answer++;
+    }
+    return answer;
+}
+
+arr = [25, 23, 11, 47, 53, 17, 33];
+console.log(solution(3, arr));
+
+arr2 = [12, 20, 54, 30, 87, 91, 30];
+console.log(solution(0, arr2));
+
+
+
