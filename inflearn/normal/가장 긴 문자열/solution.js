e272337d1d7d8c1b704da373b8d72d8f975b9014
ShinjungOh <tlswjd9260@gmail.com>
2022-06-02 16:26:47 +0900

    [ADD] 인프런: 기본문제 - 가장 긴 문자열(for문)

--- inflearn/normal/가장 긴 문자열/solution.js
+++ inflearn/normal/가장 긴 문자열/solution.js
@@ -1,9 +1,23 @@
 function solution(s) {
-    let answer, max = Number.MIN_SAFE_INTEGER;
-    for (let x of s) {
-        if (x.length > max) {
-            max = x.length;
-            answer = x;
+    // let answer, max = Number.MIN_SAFE_INTEGER;
+    // for (let x of s) {
+    //     if (x.length > max) {
+    //         max = x.length;
+    //         answer = x;
+    //     }
+    // }
+
+    // let answer = "";
+    // for (let x of s) {
+    //     if (x.length > answer.length) {
+    //         answer = x;
+    //     }
+    // }
+
+    let answer = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s[i].length > answer.length) {
+            answer = s[i];
         }
     }
     return answer;
